3f959a413929b599638718cc6257efa120853376
Guus Huizen <guushuizen@outlook.com>
2018-07-20 11:35:31 +0200

    Added register functionality, alert for successful registration, misc. js fixes/adds/removals

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller {
+
+    /**
+     * Handle a registration sent via the front-end
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function handleRegister(Request $request) {
+        $password = bcrypt($request['password']);
+
+        User::create([
+            'username' => $request['username'],
+            'email' => $request['email'],
+            'password' => $password
+        ]);
+
+        return response()->json([
+            'status' => 200
+        ])->setStatusCode(200);
+    }
+}
